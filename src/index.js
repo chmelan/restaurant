565702910d2e12616a82f10cd966836390a17b7e
chmelan <chmelan@comcast.net>
2020-05-30 15:08:42 -0400

    changed webpage to load content via JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log("hello")
+const content = document.querySelector("#content")
+content.innerHTML = ` <nav>
+<img class="navLogo" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+<span class="navPageTitle">Elmer's Eggselent Emporium</span>
+<button class="navButton" id="navAboutButton">About</button>
+<button class="navButton" id="navMenuButton">Menu</button>
+<button class="navButton" id="navContactButton">Contact</button>
+</nav>
+<div class="bodyPageTitle">
+<h1>Elmer's Eggselent Emporium</h1>
+<h2>You need eggs? We have eggs. So many eggs.</h2>
+</div>
+<div class="reviewContainer">
+<div class="reviewEgg">
+    <div class="reviewText">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <p>Wow! Abslutely eggceptional customer service!<br>-Suzie </p>
+    </div>
+</div>
+<div class="reviewEgg">
+    <div class="reviewText">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <p>The owner was too eggcentric for me.<br>-Greg</p>
+    </div>
+</div>
+<div class="reviewEgg">
+    <div class="reviewText">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <img class="eggRating" src="https://image.flaticon.com/icons/png/512/44/44506.png">
+        <p>It's eggactly what you'd expect.<br>-Nik </p>
+    </div>
+</div>
+</div>`
